9677b37216e098d624599095cca8b6245a8b67c8
Renzo Mogrovejo <rzmogrovejo@gmail.com>
2024-10-04 17:57:09 -0500

    add new logo and implement first widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import ennuLogo from './assets/ennu-co-web-logo.webp'
 import './App.css'
+import PotentialWeight from './widgets/PotentialWeight'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+{/*         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
+        </a> */}
+        <a href="https://ennu.co" target="_blank">
+          <img src={ennuLogo} className="logo ennu" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <h1>Ennu Widgets</h1>
+      <div className="div">
+        <h2>Potential Weight Widget</h2>
+        <PotentialWeight/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }

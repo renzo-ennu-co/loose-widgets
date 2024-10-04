f1e7bd3db266312158217a0ecc68734d7bfec33e
Renzo Mogrovejo <rzmogrovejo@gmail.com>
2024-10-04 17:54:23 -0500

    create specific build config for widgets

---
+++ vite.config.widgets.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        "potential-weight": "src/components/PotentialWeight/render.tsx"
+      },
+      output: {
+        dir: 'output',
+        entryFileNames: 'widgets/[name]-[hash].js',
+      },
+    },
+    //outDir: 'dist',  // Output directory for the static website
+    emptyOutDir: true,  // Clean output folder before each build
+    watch: {}
+  },     
+})

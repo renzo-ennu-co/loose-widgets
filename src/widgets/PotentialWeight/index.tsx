77c07326a1c32e4ab40fa00c86ffba40f2970dfc
Renzo Mogrovejo <rzmogrovejo@gmail.com>
2024-10-07 00:42:14 -0500

    use new styled components

--- src/widgets/PotentialWeight/index.tsx
+++ src/widgets/PotentialWeight/index.tsx
@@ -1,5 +1,17 @@
 import { useState } from 'react'
-import { Span } from "./style";
+import { 
+  BoldText,
+  Container, 
+  ContentWrapper, 
+  CurrentWeightContainer, 
+  Label, 
+  NumberInput, 
+  PotentialWeightContainer, 
+  PotentialWeightValue, 
+  Slider, 
+  SliderContainer,
+  Title
+} from "./styles";
 
 const INITIAL_WEIGHT = 252; //lbs
 const WEIGHT_LOSS_FACTOR = 0.85;
@@ -9,37 +21,34 @@ function PotentialWeight() {
   const potentialWeight = Math.round(currentWeight * WEIGHT_LOSS_FACTOR);
 
   return (
-    <>
-    <div className="flex flex-col items-center">
-        <div>
-            <div className="border-4 border-sky px-l rounded-md text-onprimary">
-                <div className="column items-center pt-s">
-                    <div className="text-l leading-1">Your potential weight with GLP-1s:</div>
-                        <div className="text-3xl whitespace-nowrap">
-                            <Span>{ potentialWeight } lbs</Span> 
-                        </div>
-                    </div>
-                </div>
-            </div> 
-            <div className="w-full max-w-[250px]">
-                <input type="range" 
-                    className="text-neutral w-full accent-orange" 
-                    min="120" 
-                    max="400" 
-                    value={currentWeight} 
-                    onChange={(e) => setCurrentWeight(Number(e.target.value))}
-                />
-            </div>
-            <div className="row items-center gap-m">
-                <label className="text-xl">Your current weight:</label>
-                <input type="number" 
-                    className="text-neutral rounded-[2px] py-xs text-center text-l border-b-4 border-neutral bg-page" 
-                    value={currentWeight}
-                    onChange={(e) => setCurrentWeight(Number(e.target.value))}
-                />
-            </div>
-        </div>
-    </>
+    <Container>
+      <PotentialWeightContainer>
+        <ContentWrapper>
+          <Title>Your potential weight with GLP-1s:</Title>
+          <PotentialWeightValue>
+            <BoldText>{potentialWeight}</BoldText> lbs
+          </PotentialWeightValue>
+        </ContentWrapper>
+      </PotentialWeightContainer>
+      <SliderContainer>
+        <Slider
+          type="range"
+          min="120"
+          max="400"
+          value={currentWeight}
+          onChange={(e) => setCurrentWeight(Number(e.target.value))}
+        />
+      </SliderContainer>
+      <CurrentWeightContainer>
+        <Label htmlFor="weight">Your current weight:</Label>
+        <NumberInput 
+          type="number"
+          value={currentWeight}
+          onChange={(e) => setCurrentWeight(Number(e.target.value))}
+          name="weight"
+        />
+      </CurrentWeightContainer>
+    </Container>
   )
 }
 

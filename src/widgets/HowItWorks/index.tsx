bfa33037c101b7846c67a41f86f390edb093031a
Renzo Mogrovejo <rzmogrovejo@gmail.com>
2024-11-02 12:50:02 -0500

    handle active item

--- src/widgets/HowItWorks/index.tsx
+++ src/widgets/HowItWorks/index.tsx
@@ -3,46 +3,46 @@ import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
 import data from "./data"
 import { AccordionContent, AccordionHeaderContainer, AccordionItemContainer, ArrowDiv, Container, NumberAndTitleContainer, NumberContainer, NumberSpan, TitleSpan } from "./styles";
 
-const AccordionItem = ({ index, title, content }: {
-  index: number,
+const AccordionItem = ({ number, title, content, isOpen, onClick }: {
+  number: number,
   title: string,
-  content: JSX.Element
+  content: JSX.Element,
+  isOpen: boolean,
+  onClick: () => void
 }) => {
-  const [isActive, setIsActive] = useState(false);
-
   return (
     <AccordionItemContainer>
-      <AccordionHeaderContainer onClick={() => setIsActive(!isActive)}>
+      <AccordionHeaderContainer onClick={onClick}>
         <NumberAndTitleContainer>
-          <NumberContainer isActive={ isActive }>
-            <NumberSpan>{index}</NumberSpan>
+          <NumberContainer isOpen={ isOpen }>
+            <NumberSpan>{number}</NumberSpan>
           </NumberContainer>
           <TitleSpan>{title}</TitleSpan>
         </NumberAndTitleContainer>
-        <ArrowDiv>{isActive ? <MdKeyboardArrowUp/> : <MdKeyboardArrowDown />}</ArrowDiv>
+        <ArrowDiv>{isOpen ? <MdKeyboardArrowUp/> : <MdKeyboardArrowDown />}</ArrowDiv>
       </AccordionHeaderContainer>
-        {isActive && <AccordionContent>{content}</AccordionContent>}
+        {isOpen && <AccordionContent>{content}</AccordionContent>}
     </AccordionItemContainer>
   );
 };
 
 const HowItWorks = () => {
-/*   const [activeIndex, setActiveIndex] = useState(-1);
+  const [activeIndex, setActiveIndex] = useState(-1);
 
   const handleItemClick = (index: number) => {
     setActiveIndex((prevIndex) => (prevIndex === index ? -1 : index));
-  }; */
+  };
 
   return (
     <Container>
       {data.map((item, index) => (
         <AccordionItem
           key={index}
-          index={index + 1}
+          number={index + 1}
           title={item.title}
           content={item.content}
-          /* isOpen={activeIndex === index}
-          onClick={() => handleItemClick(index)} */
+          isOpen={activeIndex === index}
+          onClick={() => handleItemClick(index)}
         />
       ))}
     </Container>

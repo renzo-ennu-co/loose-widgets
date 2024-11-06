3e266c509c9fb1ea7fc25a92f78376755a0d8c72
Renzo Mogrovejo <rzmogrovejo@gmail.com>
2024-11-05 20:33:38 -0500

    add new params and logic

--- src/widgets/PreApprovedMainTitle/styles.ts
+++ src/widgets/PreApprovedMainTitle/styles.ts
@@ -1,11 +1,16 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const Container = styled.div<{
+  $screenWidth: number;
+  $scale: number;
+  $widthForDesktop: number;
+  $widthForMobile: number;
+  $fontAdjustment: number;
+}>`
   font-family: "Poppins", serif;
-  width: 100%;
-  @media (min-width: 600px) {
-    width: 700px;
-  }
+  transform: ${props => props.$screenWidth > 600 ? `scale(calc(1* ${props.$scale}))` : `scale(calc(${props.$fontAdjustment/100}* ${props.$scale}))`};
+  transform-origin: 0 0;
+  width: ${props => props.$screenWidth > 600 ? `${props.$widthForDesktop}px` : `calc(${props.$widthForMobile}px/${props.$fontAdjustment/100})`};
 `;
 
 export const H1 = styled.h1`

b91ae5a3ad02d2fced85ea9e10a5b44cb644b300
Renzo Mogrovejo <rzmogrovejo@gmail.com>
2024-11-05 20:35:09 -0500

    read and pass all params from div

--- src/widgets/PreApprovedMainTitle/render.tsx
+++ src/widgets/PreApprovedMainTitle/render.tsx
@@ -4,9 +4,18 @@ import PreApprovedMainTitle from '.'
 
 const container = document.getElementById('pre-approved-main-title-widget')!
 const dataset = container.dataset;
+const name = dataset.name;
+const widthForDesktop = Number(dataset.widthForDesktop);
+const widthForMobile = Number(dataset.widthForMobile);
+const fontAdjustment = Number(dataset.fontAdjustment)
 
 createRoot(container).render(
   <StrictMode>
-    <PreApprovedMainTitle name={dataset.title} />
+    <PreApprovedMainTitle
+      name={name}
+      widthForDesktop={widthForDesktop}
+      widthForMobile={widthForMobile}
+      fontAdjustment={fontAdjustment}
+    />
   </StrictMode>,
 )
